21cf9d86ad2670aaeec9074f97cdbcee1a108493
Denny Kunker <wkunker@gmail.com>
2013-08-06 23:38:08 +0000

    Renamed elements in main.js and index.html to support more charts and options

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ var libRequest = require('request');
 var mongoose = require('mongoose');
 var config = require('./config.js');
 
-var Stats30s;
-var Stats1hr;
+var Stats30s_6h;
+var Stats1hr_24h;
 
 // Max number of statistic elements that can be sent through /stats/*
-var num30sElements = 360; // 6hr period
-var num1hrElements = 24; // 24hr period
+var num30s6hElements = 360; // 6hr period
+var num1hr24hElements = 24; // 24hr period
 
 var getLastNElements = function(arr, n) {
   return arr.slice(Math.max(arr.length - n, 0));
@@ -38,20 +38,20 @@ app.get('/', function(request, response) {
   response.send(indexPage);
 });
 
-app.get('/stats/30s', function(request, response) {
-  Stats30s.find({}, {_id:0, __v:0}, function (err, stats) {
+app.get('/stats/30s/6h', function(request, response) {
+  Stats30s_6h.find({}, {_id:0, __v:0}, function (err, stats) {
     if (!err) {
-      response.send(getLastNElements(stats, num30sElements));
+      response.send(getLastNElements(stats, num30s6hElements));
     } else {
       response.send("Error: Bityank Coinbase relay down. Please try again shortly.");
     }
   });
 });
 
-app.get('/stats/1hr', function(request, response) {
-  Stats1hr.find({}, {_id:0, __v:0}, function (err, stats) {
+app.get('/stats/1hr/24h', function(request, response) {
+  Stats1hr_24h.find({}, {_id:0, __v:0}, function (err, stats) {
     if (!err) {
-      response.send(getLastNElements(stats, num1hrElements));
+      response.send(getLastNElements(stats, num1hr24hElements));
     } else {
       response.send("Error: Bityank Coinbase relay down. Please try again shortly.");
     }
@@ -70,7 +70,7 @@ app.listen(port, function() {
   db.once('open', function callback () {
     var statsSchema30s = mongoose.Schema({"time":Number, buytotal: Number, selltotal: Number});
     var statsSchema1hr = mongoose.Schema({"timestart":Number, "timeend":Number, buytotal: Number, selltotal: Number});
-    Stats30s = mongoose.model('stats30s', statsSchema30s);
-    Stats1hr = mongoose.model('stats1hr', statsSchema1hr);
+    Stats30s_6h = mongoose.model('stats30s', statsSchema30s);
+    Stats1hr_24h = mongoose.model('stats1hr', statsSchema1hr);
   });
 });
